cc07051cb50e55fed1d61ff402abb43d5d34bf3b
john2306 <johnmendozauni@gmail.com>
2020-03-27 09:59:46 -0500

    mejoras

---
+++ playMusic.py
@@ -0,0 +1,11 @@
+import pygame
+
+from time import sleep
+
+pygame.mixer.init()
+pygame.mixer.music.load(open("laGasolina.mp3","rb"))
+pygame.mixer.music.play()
+while pygame.mixer.music.get_busy():
+    print('Playing..')
+
+print("End program")
